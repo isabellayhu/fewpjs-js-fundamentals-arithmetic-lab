61c8daabed330ca2fc5c914694b773eb3943ad83
Isabella <isabella.y.hu@gmail.com>
2022-04-08 21:07:19 +1000

    commit tree c087fe41b91ee80896b2c8cab638d1b4f580ed51

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+var num1 = 31;
+var num2 = 2;
+var multiply = num1 * num2;
+
+var random =1;
+
+var num3 = 9;
+var num4 = 5;
+var mod = num3 % num4;
+
+var max = Math.max(0,5,10,15,20);
